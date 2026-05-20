3a2d169abb3581dca059123c9b71f4863bf70869
Sebastian <sortega@alumnos.exa.unice.edu.ar>
2026-05-20 09:57:18 -0300

    Agregados componentes NavBar y ProjectCard

--- Landing page/src/App.vue
+++ Landing page/src/App.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import NavBar from './components/NavBar.vue';
+  import ProjectCard from './components/ProjectCard.vue';
+</script>
 
 <template>
   <h1>You did it!</h1>
