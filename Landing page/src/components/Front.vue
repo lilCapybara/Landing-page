fa95af98f3db88230df5257722ada9a9ea258479
Sebastian Ortega <sortega@alumnos.exa.unicen.edu.ar>
2026-05-23 12:44:19 -0300

    Agregado componente de contacto

--- Landing page/src/components/Front.vue
+++ Landing page/src/components/Front.vue
@@ -3,13 +3,15 @@
     <div id="container">
         <h1>Sebastián Ortega</h1>
         <h2>Software developer</h2>
-        <button>Ver mi curriculum</button>
-        <img src="" alt="">
     </div>
 </template>
 
 <style scoped>
-    template{
-        background-color: aliceblue;
+    #container{
+        background-color: rgb(104, 108, 110);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
 </style>
