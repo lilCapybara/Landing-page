2adb92a18e4007863433d704718d0c5ef75825c1
Sebastian <sortega@alumnos.exa.unice.edu.ar>
2026-05-28 19:40:27 -0300

    Corregido import en app.vue de LanguageSection

--- Landing page/src/components/AboutSection.vue
+++ Landing page/src/components/AboutSection.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import LanguageCard from './Languages/languageCard.vue';
+import LanguageCard from './Languages/LanguageCard.vue';
 
 </script>
 
--- Landing page/src/components/Languages/LanguagesSection.vue
+++ Landing page/src/components/Languages/LanguagesSection.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-    import LanguageCard from './languageCard.vue';
+    import LanguageCard from './LanguageCard.vue';
     
 </script>
 
